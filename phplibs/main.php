b1beba80c48f8680a3bb6c82cd697af3f8b782cc
kow sakazaki <kow@prominence.tv>
2014-01-24 02:25:28 +0900

    phplibs update

--- phplibs/main.php
+++ phplibs/main.php
@@ -8,20 +8,3 @@ $ini = parse_ini_file($_HOMEDIR_."/configs/system.ini");
 
 // add library include path
 set_include_path(get_include_path().PATH_SEPARATOR.$_HOMEDIR_."/syslibs");
-
-// setup smarty
-if (!defined("SMARTY_DIR")) {
-    define("SMARTY_DIR", $ini["smartypath"]);
-}
-
-require_once(SMARTY_DIR . "/Smarty.class.php");
-
-$smarty = new Smarty();
-$smarty->template_dir   = "./templates";
-$smarty->compile_dir    = "./templates_c";
-$smarty->config_dir     = "./configs";
-$smarty->cache_dir      = "./cache";
-// デリミタ変更
-$smarty->left_delimiter = "[{";
-$smarty->right_delimiter = "}]";
-?>
